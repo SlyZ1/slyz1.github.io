8fdfa8ead88e7a19f346adb71d61374d5cc44e77
Baptiste Girardin <baptiste.girardin@telecom-paris.fr>
2026-06-15 14:53:37 +0200

    Update hero buttons style and nav links

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,5 @@ document.getElementById('navbar').innerHTML = `
   <ul class="nav-links">
     <li><a href="index.html">Home</a></li>
     <li><a href="projects.html">Works</a></li>
-    <li><a href="index.html#skills">Skills</a></li>
   </ul>
 `;
