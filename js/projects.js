4bb1a17238603e8853a7b085555ebf8882fb6b81
Baptiste Girardin <baptiste.girardin@telecom-paris.fr>
2026-07-30 13:44:31 +0200

    Update path-tracer's card

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,14 @@
 const PROJECTS = {
   'path-tracer': {
     url: 'https://github.com/SlyZ1/Path-Tracer',
-    thumb: '<img src="https://raw.githubusercontent.com/SlyZ1/Path-Tracer/main/outputs/triple_dragon.png" alt="Path Tracer" />',
+    //thumb: '<img src="https://raw.githubusercontent.com/SlyZ1/Path-Tracer/main/outputs/triple_dragon.png" alt="Path Tracer" />',
+    carousel: [
+      "assets/path-tracing/triple_dragon.png",
+      "assets/path-tracing/spectral_example.png",
+      "assets/path-tracing/bunny_fur.png"
+    ],
+    thumbClass: '',
+    //maxThumbHeight: [160, null],
     title: 'Spectral & RGB Path Tracer',
     date: 'Nov 2025 - Now',
     wip: true,
